f32a94b024a3aa9f96c4ac5e5360ee7b0484236f
Razmik Manvelyan <58045200+razmikmanvelyan@users.noreply.github.com>
2023-11-22 16:53:10 +0400

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+﻿// Design Patterns.cpp : Defines the entry point for the application.
+//
+
+#include "Singleton/SingletonTest.h"
+#include "Builder/BuilderTest.h"
+#include "Prototype/PrototypeTest.h"
+#include "Factory/FactoryTest.h"
+#include "Abstract Factory/AbstractFactoryTest.h"
+#include "Adapter/AdapterTest.h"
+#include "Composite/CompositeTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << testIsSingleInstance() << endl;
+	testBuilder();
+	testPrototype();
+	testFactory();
+	testAbstractFactory();
+	testAdapter();
+	testComposite();
+	return 0;
+}
